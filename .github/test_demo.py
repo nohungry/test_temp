38d2d6ac977b69e0d67d218ae336e63ccaf69471
norman_cheng <norman_cheng@paradise-soft.com.tw>
2023-05-24 17:48:41 +0800

    test

---
+++ .github/test_demo.py
@@ -0,0 +1,13 @@
+def test_export_report_1():
+    a = 1 + 1
+    b = 2 + 2
+    
+    assert b > a
+    
+def test_export_report_2():
+    a = 2 + 2
+    b = 4 + 4
+    
+    assert a < b
+    
+    
